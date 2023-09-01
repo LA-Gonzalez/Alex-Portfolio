0e502519c1f5bd166b39445ea6b2c152aa1d1e25
Alex Gonzalez <l.alejandro.gonzalez@proton.me>
2023-09-01 17:59:28 -0400

    Initial App Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HeroSection from './components/HeroSection'
-import AboutMe from './components/AboutMe'
-import Contact from './components/Contact'
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <HeroSection />
       <AboutMe />
       <Projects />
       <Contact />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
