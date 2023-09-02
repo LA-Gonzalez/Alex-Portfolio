2d0fe71800e2364ddb2d57ab6769c3d970205542
Alex Gonzalez <l.alejandro.gonzalez@proton.me>
2023-09-02 14:09:02 -0400

    Added functionality to hero section button

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+
+import "./HeroSection.scss";
 
 const HeroSection = () => {
-  return (
-    <div>HeroSection</div>
-  )
-}
+  // Function to scroll to the AboutMe section
+  const scrollToAboutMe = () => {
+    const aboutMeSection = document.getElementById("about-me-section");
+    if (aboutMeSection) {
+      window.scrollTo({
+        top: aboutMeSection.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  };
 
-export default HeroSection
+  // Attach the scroll function to the button click event
+  const handleButtonClick = () => {
+    scrollToAboutMe();
+  };
+  return (
+    <div id="hero-section" className="portfolio-section hero-section-container">
+      <div className="hero-text-container">
+        <h1>
+          Hi, I'm Alex Gonzalez,
+          <br />
+          Web Developer
+        </h1>
+        <text>
+          insert cool background here, special effects would be also nice
+        </text>
+      </div>
+
+      <button onClick={handleButtonClick} >Check out my page!</button>
+    </div>
+  );
+};
+
+export default HeroSection;
