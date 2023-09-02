3c1fab40137ae4cd1effe4de9523a0461ed05567
Alex Gonzalez <l.alejandro.gonzalez@proton.me>
2023-09-02 15:12:37 -0400

    Formspree contact form added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [state, handleSubmit] = useForm("mzblgrrn");
+  if (state.succeeded) {
+    return (
+      <p>
+        Thank you for your interest, I'll be sure to get back to you as soon as
+        possible!
+      </p>
+    );
+  }
 
-export default Contact
+  return (
+    <div id="contact-section" className="portfolio-section">
+      <h2>Contact Me</h2>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" name="name" />
+        <label htmlFor="email">Email Address</label>
+        <input id="email" type="email" name="email" />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <label htmlFor>Your Message</label>
+        <textarea id="message" name="message" />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button type="submit" disabled={state.submitting}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
