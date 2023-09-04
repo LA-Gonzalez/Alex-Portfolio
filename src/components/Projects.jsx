2244c6ebc10b42910796d0b24be09bba0b250298
Alex Gonzalez <l.alejandro.gonzalez@proton.me>
2023-09-04 19:04:43 -0400

    added id to projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // A carousel of projects, we'll focus on 3
 const Projects = () => {
-  return <div className="portfolio-section">Projects</div>;
+  return <div id="projects-section" className="portfolio-section">Projects</div>;
 };
 
 export default Projects;
