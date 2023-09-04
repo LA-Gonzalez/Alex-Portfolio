23955ac245a6502294678d319f581ab4b5031c6e
Alex Gonzalez <l.alejandro.gonzalez@proton.me>
2023-09-04 19:04:29 -0400

    scroll handler

---
+++ src/utils/scrollHandler.js
@@ -0,0 +1,11 @@
+
+export const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      window.scrollTo({
+        top: section.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  };
+  
